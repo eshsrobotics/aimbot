4a9b2da5ae73c1b68c7659bc261ab3399bf0db62
Zach Latta <zach@zachlatta.com>
2014-01-24 11:55:48 -0800

    Work from past meeting.

--- robot.py
+++ robot.py
@@ -5,21 +5,14 @@ try:
 except ImportError:
   from pyfrc import wpilib
 
-SPEED_LIMIT = 1.5
-
 stick = wpilib.Joystick(1)
 
-class MyRobot(wpilib.SimpleRobot):
-  
-  def __init__(self):
-    super().__init__()
+frontRightMotor = wpilib.Jaguar(1)
+frontLeftMotor = wpilib.Jaguar(2)
+backLeftMotor = wpilib.Jaguar(3)
+backRightMotor = wpilib.Jaguar(4)
 
-    self.stick = wpilib.Joystick(1)
-    
-    self.frontRightMotor = wpilib.Jaguar(1)
-    self.frontLeftMotor = wpilib.Jaguar(2)
-    self.backLeftMotor = wpilib.Jaguar(3)
-    self.backRightMotor = wpilib.Jaguar(4)
+class MyRobot(wpilib.SimpleRobot):
 
   def Autonomous(self):
   
@@ -36,9 +29,9 @@ class MyRobot(wpilib.SimpleRobot):
     while self.IsOperatorControl() and self.IsEnabled():
       dog.Feed()
 
-      forward = -self.stick.GetY() / SPEED_LIMIT
-      right = self.stick.GetX() / SPEED_LIMIT
-      clockwise = self.stick.GetZ() / SPEED_LIMIT
+      forward = -stick.GetY()
+      right = stick.GetX()
+      clockwise = stick.GetZ()
 
       front_left = forward + clockwise + right
       front_right = forward - clockwise - right
@@ -58,6 +51,12 @@ class MyRobot(wpilib.SimpleRobot):
         front_right /= maxVal
         back_left /= maxVal
         back_right /= maxVal
+
+      frontRightMotor.Set(front_right)
+      frontLeftMotor.Set(front_left)
+      backLeftMotor.Set(back_left)
+      backRightMotor.Set(back_right)
+
 
 def run():
   robot = MyRobot()
