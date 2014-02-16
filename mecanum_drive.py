84aa7e26f2d6750c9dc6b62c45e041ed1d0f3c2e
MaxWofford <max.speed.wofford@gmail.com>
2014-02-16 14:25:59 -0800

    made drive the right way

--- mecanum_drive.py
+++ mecanum_drive.py
@@ -14,10 +14,10 @@ class MecanumDrive():
     right = self.stick.GetX()
     clockwise = self.stick.GetZ() * 0.5
 
-    front_right_val = forward - clockwise - right
-    back_right_val = forward - clockwise + right
-    front_left_val = forward + clockwise + right
-    back_left_val = forward + clockwise - right
+    front_right_val = forward - clockwise + right
+    back_right_val = forward - clockwise - right
+    front_left_val = forward + clockwise - right
+    back_left_val = forward + clockwise + right
 
     maxVal = math.fabs(front_left_val)
     if math.fabs(front_right_val) > maxVal:
