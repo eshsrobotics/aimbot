e997b967792f807e67e8d34e60b2160aa5069e51
Zach Latta <zach@zachlatta.com>
2014-02-17 20:11:43 -0800

    spaces > tabs

--- intake.py
+++ intake.py
@@ -1,16 +1,16 @@
 class Intake():
 
-	def __init__(self,wheels,arm,stick):
-		self.wheels = wheels
-		self.arm = arm
-		self.stick = stick
+  def __init__(self,wheels,arm,stick):
+    self.wheels = wheels
+    self.arm = arm
+    self.stick = stick
 
-	def iterate(self):
-		self.arm.Set(self.stick.GetY())
-		wheels = 0
+  def iterate(self):
+    self.arm.Set(self.stick.GetY())
+    wheels = 0
 
-		if self.stick.GetRawButton(3):
-			wheels = 1
-		elif self.stick.GetRawButton(4):
-			wheels = -1
-		self.wheels.Set(wheels);
+    if self.stick.GetRawButton(3):
+      wheels = 1
+    elif self.stick.GetRawButton(4):
+      wheels = -1
+    self.wheels.Set(wheels);
