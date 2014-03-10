30ee8f7bae6b0e5f51fe9d9096836cf87f6e3ce8
Zach Latta <zach@zachlatta.com>
2014-03-10 15:50:42 -0700

    Remove encoder log line.

--- shooter.py
+++ shooter.py
@@ -1,5 +1,6 @@
 SERVO_TOP_VALUE = 1 #needs to be actually found
 SERVO_BOTTOM_VALUE = 0 #needs to be actually found
+
 class Shooter():
   def __init__(self,main_motor,encoder,servo,stick):
     self.main_motor = main_motor
@@ -13,7 +14,7 @@ class Shooter():
       self.servo_enabled = True
     elif self.stick.GetRawButton(4):
       self.servo_enabled = False
-    print encoder
+
     if self.servo_enabled:
       self.servo.Set(SERVO_TOP_VALUE)
     else:
